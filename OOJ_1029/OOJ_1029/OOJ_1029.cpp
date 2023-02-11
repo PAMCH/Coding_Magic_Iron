9c065003758e4c91afe3452e3e25cd0e60c351ab
PAMCH <pamic7124@naver.com>
2023-02-11 19:20:21 +0900

    Solved

---
+++ OOJ_1029/OOJ_1029/OOJ_1029.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int i, n, m;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &m);
+		printf("%d %d\n", (m + 50) / 100 * 100, m / 100 * 100);
+	}
+
+	return 0;
+}
