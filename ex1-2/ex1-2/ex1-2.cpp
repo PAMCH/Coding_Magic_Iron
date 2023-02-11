c1e175c19ec5d49d4bb888698be7ff833742b9e1
PAMCH <pamic7124@naver.com>
2023-02-11 17:55:04 +0900

    Solved

---
+++ ex1-2/ex1-2/ex1-2.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int arr[11] = { 0,7,-5,4,-99,45,11,0,8,50,77 };
+	int i, max, min;
+
+	max = min = arr[1];
+
+	for (i = 2; i <= 10; i++) {
+		max = max < arr[i] ? arr[i] : max;
+		min = min > arr[i] ? arr[i] : min;
+	}
+
+	printf("%d\n%d\n", max, min);
+
+	return 0;
+}
