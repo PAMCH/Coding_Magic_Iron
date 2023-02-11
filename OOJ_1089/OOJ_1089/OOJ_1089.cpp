3a2152ba7a44680c6e6c7c04ca51aae03c992b5c
PAMCH <pamic7124@naver.com>
2023-02-11 18:03:03 +0900

    Solved

---
+++ OOJ_1089/OOJ_1089/OOJ_1089.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int D, H, M;
+	int m1, m2;
+
+	scanf("%d %d %d", &D, &H, &M);
+
+	m1 = 11 * 24 * 60 + 11 * 60 + 11;
+	m2 = D * 24 * 60 + H * 60 + M;
+
+	printf("%d\n", m2 - m1 < 0 ? -1 : m2 - m1);
+
+	return 0;
+}
