fbed4c195a84045a950e860394f9058c0ff93b79
PAMCH <pamic7124@naver.com>
2023-02-11 19:59:38 +0900

    Solved

---
+++ OOJ_1083/OOJ_1083/OOJ_1083.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int r, m, y, i;
+	double money;
+
+	scanf("%d %d %d", &r, &m, &y);
+
+	money = m;
+
+	for (i = 1; i <= y; i++) {
+		money *= (1 + 0.01 * r);
+	}
+
+	printf("%d\n", int(money));
+
+	return 0;
+}
