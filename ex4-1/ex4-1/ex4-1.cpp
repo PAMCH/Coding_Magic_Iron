734be5d4877787cbef04a749e1d8e480104370ea
PAMCH <pamic7124@naver.com>
2023-02-11 19:15:05 +0900

    Solved

---
+++ ex4-1/ex4-1/ex4-1.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	printf("%d\n%d\n%d\n%d\n", int(round(n / 10.0) * 10), int(ceil(n / 10.0) * 10), int(floor(n / 10.0) * 10), int(trunc(n / 10.0) * 10));
+
+	return 0;
+}
