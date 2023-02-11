58273f7f65983136a1625bcf10800995e5124562
PAMCH <pamic7124@naver.com>
2023-02-11 19:30:03 +0900

    Solved

---
+++ OOJ_1065/OOJ_1065/OOJ_1065.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int i, n, s, sum = 0, cnt = 0;
+	double ave;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &s);
+		if (s <= 0) {
+			cnt++;
+		}
+		else {
+			sum += s;
+		}
+	}
+
+	n -= cnt;
+
+	ave = double(sum) / n;
+
+	printf("%d\n", int(ave * 10 + 0.5));
+
+	return 0;
+}
