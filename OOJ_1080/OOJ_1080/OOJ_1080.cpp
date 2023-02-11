689606bbbdef660f38dc974825b15ef271c1d1ff
PAMCH <pamic7124@naver.com>
2023-02-11 19:56:56 +0900

    Solved

---
+++ OOJ_1080/OOJ_1080/OOJ_1080.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, t;
+	double tax;
+
+	scanf("%d", &n);
+	scanf("%d", &t);
+	
+	tax = n * (t * 0.01);
+	printf("%.0lf\n", round(tax));
+
+	return 0;
+}
