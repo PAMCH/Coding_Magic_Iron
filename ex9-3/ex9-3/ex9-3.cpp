928e8d6d1d407cc444c24e86d312dacfd310f9c9
PAMCH <pamic7124@naver.com>
2023-02-12 22:58:14 +0900

    Solved

---
+++ ex9-3/ex9-3/ex9-3.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n,i;
+	long long a[91] = { 0,1 };
+
+	scanf("%d", &n);
+
+	for (i = 2; i <= n; i++) {
+		a[i] = a[i - 1] + a[i - 2];
+	}
+
+	printf("%lld\n", a[n]);
+
+	return 0;
+}
