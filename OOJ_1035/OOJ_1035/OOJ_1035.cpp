790a1427b67844277f449ebf046392f68b109335
PAMCH <pamic7124@naver.com>
2023-02-12 15:59:00 +0900

    Solved

---
+++ OOJ_1035/OOJ_1035/OOJ_1035.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int grade[101] = { 0 }, score[101];
+	int i, j, n;
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &score[i]);
+	}
+
+	for (i = 1; i <= n; i++) {
+		grade[i] = 1;
+		for (j = 1; j <= n; j++) {
+			if (score[i] < score[j]) {
+				grade[i]++;
+			}
+		}
+	}
+
+	for (i = 1; i <= n; i++) {
+		printf("%d ", grade[i]);
+	}
+
+	return 0;
+}
