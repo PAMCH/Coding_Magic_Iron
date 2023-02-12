f9c219b28f3e5709313e522d1ff38f289a07ab20
PAMCH <pamic7124@naver.com>
2023-02-12 16:03:39 +0900

    Solved

---
+++ OOJ_1151/OOJ_1151/OOJ_1151.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, i, max, cnt = 0;
+	int arr[100001];
+
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	max = -1;
+
+	for (i = 1; i <= n; i++) {
+		if (max < arr[i]) {
+			max = arr[i], cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
