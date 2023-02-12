1c5289fafbf327ea36a9b51ae1d5d654bc7acea6
PAMCH <pamic7124@naver.com>
2023-02-12 17:01:36 +0900

    Solved

---
+++ OOJ_1040/OOJ_1040/OOJ_1040.cpp
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[101][101];
+	int n, m, i, j;
+
+	scanf("%d %d", &n, &m);
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= m; j++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	for (j = 1; j <= m; j++) {
+		for (i = 1; i <= n; i++) {
+			printf("%d ", a[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
