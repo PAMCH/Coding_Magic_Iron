94fb1757f1ac3d48af1ec0c5122e5e8af7571b8e
PAMCH <pamic7124@naver.com>
2023-02-12 23:31:05 +0900

    Solved

---
+++ OOJ_1129/OOJ_1129/OOJ_1129.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	double a[21], temp;
+	int n, i, j, k;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		scanf("%lf", &a[i]);
+	}
+
+	for (k = 1; k <= n; k++) {
+		for (i = k; i <= n - 1; i++) {
+			for (j = 1 + i; j <= n; j++) {
+				if (a[i] > a[j]) {
+					temp = a[i], a[i] = a[j], a[j] = temp;
+				}
+			}
+		}
+
+		a[k + 1] = (a[k] + a[k + 1]) / 2;
+	}
+
+	printf("%lf\n", a[n]);
+
+	return 0;
+}
