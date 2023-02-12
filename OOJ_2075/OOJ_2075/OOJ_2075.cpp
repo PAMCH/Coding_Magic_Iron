b127d33cc02d03b197f7e46944d7e02fe58b59d8
PAMCH <pamic7124@naver.com>
2023-02-12 18:13:56 +0900

    Solved

---
+++ OOJ_2075/OOJ_2075/OOJ_2075.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, x, c[101] = { 0 };
+	int i, j, idx = 1;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			scanf("%d", &x);
+			if (x == 3) {
+				c[i]++;
+			}
+		}
+	}
+
+	for (i = 2; i <= n; i++) {
+		if (c[idx] < c[i]) {
+			idx = i;
+		}
+	}
+
+	printf("%d\n", idx);
+
+	return 0;
+}
