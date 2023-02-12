829db40f0a1bb509af5dfdc6fb90c7d138700b54
PAMCH <pamic7124@naver.com>
2023-02-12 23:39:43 +0900

    Solved

---
+++ OOJ_4171/OOJ_4171/OOJ_4171.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	double r;
+	scanf("%lf", &r);
+
+	printf("%lf\n%lf\n", r * r * 3.141592653589793, r * r * 2);
+
+	return 0;
+}
