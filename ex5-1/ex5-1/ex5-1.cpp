8e5c52b44b25d969f2fd31a5585c94d36364ec6d
PAMCH <pamic7124@naver.com>
2023-02-12 15:48:44 +0900

    Solved

---
+++ ex5-1/ex5-1/ex5-1.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int score[6], grade[6] = { 0 };
+	int i, j, x, y, z;
+
+	for (i = 1; i <= 5; i++) {
+		scanf("%d %d %d", &x, &y, &z);
+		score[i] = x + y + z;
+	}
+
+	for (i = 1; i <= 5; i++) {
+		grade[i] = 1;
+			for (j = 1; j <= 5; j++) {
+				if (score[i] < score[j]) {
+					grade[i]++;
+				}
+			}
+	}
+
+	for (i = 1; i <= 5; i++) {
+		printf("%d\n", grade[i]);
+	}
+
+	return 0;
+}
