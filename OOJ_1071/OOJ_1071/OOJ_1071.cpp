374d862cc2044cd21a95a154c0e630d3b787592e
PAMCH <pamic7124@naver.com>
2023-02-12 23:01:23 +0900

    Solved

---
+++ OOJ_1071/OOJ_1071/OOJ_1071.cpp
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	long long a, b;
+	scanf("%lld %lld", &a, &b);
+
+	printf("%lld\n", a > b ? a - b : b - a);
+
+	return 0;
+}
