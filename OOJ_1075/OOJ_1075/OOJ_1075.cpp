d3daf2e28737f434f10da0b818083c33b1e28f9c
PAMCH <pamic7124@naver.com>
2023-02-12 18:01:03 +0900

    Solved

---
+++ OOJ_1075/OOJ_1075/OOJ_1075.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[21][21] = { 0 };
+	int n, s, i, j;
+
+	scanf("%d %d", &n, &s);
+
+	for (j = 1; j <= n; j++) {
+		for (i = 1; i <= j; i++) {
+			a[i][j] = s;
+			s = s % 9 + 1;
+		}
+	}
+
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n; j++) {
+			if (a[i][j] == 0) {
+				printf("  ");
+			}
+			else {
+				printf("%d ", a[i][j]);
+			}
+
+		}
+		printf("\n");
+
+	}
+
+	return 0;
+}
