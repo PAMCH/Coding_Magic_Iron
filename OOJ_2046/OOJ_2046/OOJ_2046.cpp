324b349e27ef9944741f1faf1d1b97543dc8e392
PAMCH <pamic7124@naver.com>
2023-02-12 23:10:18 +0900

    Solved

---
+++ OOJ_2046/OOJ_2046/OOJ_2046.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, arr[100001];
+	int i, x, y;
+
+	scanf("%d", &n);
+
+	for (i = 1; i <= n; i++) {
+		scanf("%d %d", &x, &y);
+		arr[i] = x + y;
+	}
+
+	for (i = n; i >= 1; i--) {
+		arr[i - 1] += arr[i] / 10;
+		arr[i] %= 10;
+	}
+
+	for (i = 1; i <= n; i++) {
+		printf("%d", arr[i]);
+		if (i % 100 == 0) {
+			printf("\n");
+		}
+	}
+
+	return 0;
+}
