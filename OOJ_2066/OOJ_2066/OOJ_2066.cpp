3c8d77629e8d90095d22fa5b718278cb973ae01a
PAMCH <pamic7124@naver.com>
2023-02-12 18:05:26 +0900

    Solved

---
+++ OOJ_2066/OOJ_2066/OOJ_2066.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[21][21];
+	int r, c, i, j;
+
+	scanf("%d %d", &r, &c);
+	for (j = 1; j <= c; j++) {
+		for (i = 1; i <= r; i++) {
+			scanf("%d", &a[i][j]);
+		}
+	}
+
+	for (i = 1; i <= r; i++) {
+		for (j = 1; j <= c; j++) {
+			printf("%d\n", a[i][j]);
+		}
+	}
+
+	return 0;
+}
