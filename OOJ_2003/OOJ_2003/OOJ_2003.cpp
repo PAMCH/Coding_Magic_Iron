63e4622344bd19fc5e3200a0ffa98862e3d91589
PAMCH <pamic7124@naver.com>
2023-02-12 18:31:45 +0900

    Solved

---
+++ OOJ_2003/OOJ_2003/OOJ_2003.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[11][11][11], m, r, c, h, i, j, row[11] = { 0 };
+
+	scanf("%d %d %d", &m, &r, &c);
+	for (h = 1; h <= m; h++) {
+		for (i = 1; i <= r; i++) {
+			for (j = 1; j <= c; j++) {
+				scanf("%d", &a[h][i][j]);
+				row[i] += a[h][i][j];
+			}
+		}
+	}
+
+	for (i = 1; i <= r; i++) {
+		printf("%d ", row[i]);
+	}
+
+	return 0;
+}
