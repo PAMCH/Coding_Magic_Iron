b5c9806059c837124d6648f9a72fd4aa78a26598
PAMCH <pamic7124@naver.com>
2023-02-12 22:55:00 +0900

    Solved

---
+++ ex9-2/ex9-2/ex9-2.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, b = 1, d = 0;
+	scanf("%d", &n);
+
+	while (n)
+	{
+		d += b * (n % 10);
+		b *= 2;
+		n /= 10;
+	}
+
+	printf("%d\n", d);
+
+	return 0;
+}
