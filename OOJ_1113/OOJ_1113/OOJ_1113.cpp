b6c4f62d4110c74c4312a682d7c5755a458256a4
PAMCH <pamic7124@naver.com>
2023-02-12 23:05:02 +0900

    Solved

---
+++ OOJ_1113/OOJ_1113/OOJ_1113.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n, m, a;
+	long long r, c;
+
+	scanf("%d %d %d", &n, &m, &a);
+
+	r = (n - 1) / a + 1;
+	c = (m - 1) / a + 1;
+
+	printf("%lld\n", r * c);
+
+	return 0;
+}
