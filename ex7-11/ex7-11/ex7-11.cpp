b7ced07bb90793f1a8bdfa6458c198ca3ccaeb6e
PAMCH <pamic7124@naver.com>
2023-02-12 17:40:47 +0900

    Solved

---
+++ ex7-11/ex7-11/ex7-11.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[6][6] = { {0},{0,1,2,3,4,5},
+		{0,6,7,8,9,10},
+		{0,11,12,13,14,15},
+		{0,16,17,18,19,20},
+		{0,21,22,23,24,25}
+	};
+	int i, j, sum = 0;
+
+	for (i = 1; i <= 5; i++) {
+		for (j = 1; j <= 5; j++) {
+			if ((i + j) % 2 == 0)
+				sum += a[i][j];
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
