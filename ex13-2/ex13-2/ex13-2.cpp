5a6844529986c12611cd210c8f1992a1b87c483a
PAMCH <pamic7124@naver.com>
2023-02-13 21:57:16 +0900

    Solved

---
+++ ex13-2/ex13-2/ex13-2.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int divisor(int x) {
+    int i, sum = 0;
+
+    for (i = 1; i <= x; i++) {
+        if (x % i == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+
+int main()
+{
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+
+    printf("%d\n%d\n%d\n", divisor(a), divisor(b), divisor(c));
+
+    return 0;
+}
