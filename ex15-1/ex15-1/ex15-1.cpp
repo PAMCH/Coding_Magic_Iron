c3d9c18b0dc706a191300aa9f0055b1dc2596937
PAMCH <pamic7124@naver.com>
2023-02-13 23:20:41 +0900

    Solved

---
+++ ex15-1/ex15-1/ex15-1.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int n;
+
+void divisor() {
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+}
+
+void prime() {
+    int cnt, sum = 0;
+
+    for (int i = 1; i <= n; i++) {
+        cnt = 0;
+        for (int j = 1; j <= i; j++) {
+            if (i % j == 0) {
+                cnt++;
+            }
+        }
+
+        if (cnt == 2) {
+            sum += i;
+        }
+    }
+    printf("%d\n", sum);
+}
+
+int main()
+{
+    scanf("%d", &n);
+
+    divisor();
+    prime();
+
+    return 0;
+}
