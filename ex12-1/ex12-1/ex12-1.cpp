bdd5e4989419b9c328e8f647ccf058d8cbd0b8c2
PAMCH <pamic7124@naver.com>
2023-02-13 21:27:54 +0900

    Solved

---
+++ ex12-1/ex12-1/ex12-1.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    int a[10001] = { 0,1 };
+    int i, n, MOD = 1000000000;
+
+    scanf("%d", &n);
+
+    for (i = 2; i <= n; i++) {
+        a[i] = (a[i - 1] + a[i - 2]) % MOD;
+    }
+
+    printf("%d\n", a[n]);
+
+    return 0;
+}
