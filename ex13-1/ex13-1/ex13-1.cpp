db1227a05296e9d77189d70611abb854ea8a1e49
PAMCH <pamic7124@naver.com>
2023-02-13 21:53:11 +0900

    Solved

---
+++ ex13-1/ex13-1/ex13-1.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+void prime(int x) {
+    int i, cnt = 0;
+
+    for (i = 1; i <= x; i++) {
+        if (x % i == 0) {
+            cnt++;
+        }
+    }
+
+    if (cnt == 2) {
+        printf("PRIME\n");
+    }
+    else {
+        printf("NO PRIME\n");
+    }
+}
+
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    prime(a);
+    prime(b);
+    prime(c);
+
+    return 0;
+}
