f96970316a967497ebd729a71d42d411d738972a
PAMCH <pamic7124@naver.com>
2023-02-13 21:30:17 +0900

    Solved

---
+++ OOJ_2020/OOJ_2020/OOJ_2020.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    int n, i, r = 1, MOD = 100;
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        r = (r * 2) % MOD;
+    }
+
+    printf("%d\n", r);
+
+    return 0;
+}
