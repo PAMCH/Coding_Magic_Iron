fd21126391ebf80cb163c7c68f46ca2374f96246
PAMCH <pamic7124@naver.com>
2023-02-13 20:56:32 +0900

    Solved

---
+++ ex11-3/ex11-3/ex11-3.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d\n", n << 1);
+
+    return 0;
+}
