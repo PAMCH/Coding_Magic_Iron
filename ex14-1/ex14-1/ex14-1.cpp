fbb41deb6d5089377a9f967985737e820d58800a
PAMCH <pamic7124@naver.com>
2023-02-13 22:33:59 +0900

    Solved

---
+++ ex14-1/ex14-1/ex14-1.cpp
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+void border(int x) {
+    printf("+");
+
+    for (int i = 1; i <= x; i++) {
+        printf("-");
+    }
+
+    printf("+\n");
+}
+
+void body(int x) {
+    printf("|");
+
+    for (int i = 1; i <= x; i++) {
+        printf("-");
+    }
+
+    printf("|\n");
+}
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    border(n);
+
+    for (int i = 1; i <= n - 2; i++) {
+        body(n);
+    }
+
+    border(n);
+
+    return 0;
+}
