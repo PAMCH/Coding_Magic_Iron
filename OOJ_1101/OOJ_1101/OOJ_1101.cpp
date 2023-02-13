d47f72af87c576718b0be5cc63aa718b7c991df7
PAMCH <pamic7124@naver.com>
2023-02-13 22:04:23 +0900

    Solved

---
+++ OOJ_1101/OOJ_1101/OOJ_1101.cpp
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+void divisor(int x) {
+    int i, sum = 0;
+
+    for (i = 1; i < x; i++) {
+        if (x % i == 0) {
+            sum += i;
+        }
+    }
+
+    if (sum == x) {
+        printf("PERFECT\n");
+    }
+    else if (sum > x) {
+        printf("ABUNDANT\n");
+    }
+    else {
+        printf("DEFICIENT\n");
+    }
+}
+
+
+int main()
+{
+    int a, b, c;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    divisor(a);
+    divisor(b);
+    divisor(c);
+
+    return 0;
+}
