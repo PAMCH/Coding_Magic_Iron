569a7fe074b54aecf65fe6295cbcb6750accb1ad
PAMCH <pamic7124@naver.com>
2023-02-13 23:25:53 +0900

    Solved

---
+++ OOJ_2028/OOJ_2028/OOJ_2028.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+#define N 2000000
+
+int c, n, check[N + 1];
+
+int main()
+{
+    scanf("%d %d", &c, &n);
+
+    for (int i = 1; i <= c; i++) {
+        int t;
+        scanf("%d", &t);
+        for (int j = 0; j <= n; j += t) {
+            check[j] = 1;
+        }
+    }
+
+    int cnt = 0;
+    for (int i = 1; i <= n; i++) {
+        cnt += check[i];
+    }
+    printf("%d\n", cnt);
+
+    return 0;
+}
