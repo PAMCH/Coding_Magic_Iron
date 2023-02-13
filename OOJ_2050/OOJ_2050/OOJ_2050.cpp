7ac2d828e5cf14d8c5892ef8c945ed41adb163b9
PAMCH <pamic7124@naver.com>
2023-02-13 21:13:10 +0900

    Solved

---
+++ OOJ_2050/OOJ_2050/OOJ_2050.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    int n, x, cnt1 = 0, cnt2 = 0;
+
+    scanf("%d", &n);
+
+    x = n;
+
+    while (x)
+    {
+        if (x % 10 == 0)
+            cnt1++;
+        else
+            break;
+        x /= 10;
+    }
+
+    x = n;
+
+    while (x)
+    {
+        if ((x & 1) == 0)
+            cnt2++;
+        else
+            break;
+        x >>= 1;
+    }
+
+    printf("%d %d\n", cnt1, cnt2);
+
+    return 0;
+}
