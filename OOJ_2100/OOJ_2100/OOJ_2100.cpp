07bc79ea4eeb68e055388a67d1572353a4d594ed
PAMCH <pamic7124@naver.com>
2023-02-13 22:41:52 +0900

    Solved

---
+++ OOJ_2100/OOJ_2100/OOJ_2100.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int divisor(int x) {
+    int sum = 0;
+
+    for (int i = 1; i < x; i++) {
+        if (x % i == 0) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    while (1) {
+        int sum1 = divisor(n);
+        int sum2 = divisor(sum1);
+
+        if (sum1 != n && sum2 == n) {
+            printf("%d %d\n", n, sum1);
+            break;
+        }
+
+        n++;
+    }
+    return 0;
+}
