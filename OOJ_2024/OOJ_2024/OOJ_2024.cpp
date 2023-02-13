db7409a114574b31655045d0d20e04053a006b2e
PAMCH <pamic7124@naver.com>
2023-02-13 21:06:26 +0900

    Solved

---
+++ OOJ_2024/OOJ_2024/OOJ_2024.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    int n, i, j, temp, dir = 0;
+    int s[1001], d[1001], c[1001];
+
+    scanf("%d", &n);
+
+    for (i = 1; i <= n - 1; i++) {
+        scanf("%d %d %d", &s[i], &d[i], &c[i]);
+    }
+
+    for (i = 1; i <= n - 2; i++) {
+        for (j = i + 1; j <= n - 1; j++) {
+            if (s[i] > s[j]) {
+                temp = s[i], s[i] = s[j], s[j] = temp;
+                temp = d[i], d[i] = d[j], d[j] = temp;
+                temp = c[i], c[i] = c[j], c[j] = temp;
+            }
+        }
+    }
+
+    for (i = 1; i <= n - 1; i++) {
+        dir ^= c[i];
+    }
+
+    printf("%d\n", dir);
+
+    return 0;
+}
