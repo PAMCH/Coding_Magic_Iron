46008034487bfd166c9749a124c5e66c3f9b53c9
PAMCH <pamic7124@naver.com>
2023-02-13 22:54:17 +0900

    Solved

---
+++ OOJ_2048/OOJ_2048/OOJ_2048.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int digiMul(int x) {
+    int mul = 1;
+	while (x)
+	{
+		if (x % 10 != 0) {
+			mul *= x % 10;
+		}
+		x /= 10;
+	}
+
+	return mul;
+}
+
+int main()
+{
+	int n;
+
+	scanf("%d", &n);
+
+	while (n >= 10) {
+		printf("%d ", n);
+		n = digiMul(n);
+		printf("%d\n", n);
+	}
+
+    return 0;
+}
