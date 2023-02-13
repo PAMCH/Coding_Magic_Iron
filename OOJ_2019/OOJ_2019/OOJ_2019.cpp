66d98874a2b67419909fa28d93e5e5db4ba8d2a9
PAMCH <pamic7124@naver.com>
2023-02-13 21:34:17 +0900

    Solved

---
+++ OOJ_2019/OOJ_2019/OOJ_2019.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    int a, b, v, day;
+
+    scanf("%d %d %d", &a, &b, &v);
+
+    if ((v - a) % (a - b) == 0) {
+        day = (v - a) / (a - b);
+    }
+    else {
+        day = (v - a) / (a - b) + 1;
+    }
+
+    printf("%d\n", day + 1);
+
+    return 0;
+}
