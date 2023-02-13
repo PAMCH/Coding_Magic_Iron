fd7e5a6b1b6dd24cbecc133e2b9cf30520c99ff3
PAMCH <pamic7124@naver.com>
2023-02-13 20:58:08 +0900

    Solved

---
+++ ex11-4/ex11-4/ex11-4.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int main()
+{
+    char a = 0x7f;
+    short b = 0x7fff;
+    int c = 0x7fffffff;
+
+    printf("%hhd\n%hd\n%d\n", a, b, c);
+
+    return 0;
+}
