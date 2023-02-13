96e46c8b399fa010b5c0931bb9dee2089c71d549
PAMCH <pamic7124@naver.com>
2023-02-13 22:46:35 +0900

    Solved

---
+++ OOJ_2068/OOJ_2068/OOJ_2068.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <climits>
+
+int div(int x) {
+    for (int i = 2; i <= x; i++) {
+        if (x % i == 0) {
+            return i;
+        }
+    }
+    return 1;
+}
+
+int main()
+{
+    int n;
+
+    scanf("%d", &n);
+
+    while (n != 1) {
+        int num = div(n);
+        printf("%d\n", num);
+        n /= num;
+    }
+
+    return 0;
+}
