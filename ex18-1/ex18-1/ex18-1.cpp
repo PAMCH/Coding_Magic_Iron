75e47830999c7c31853f91d232825d66e59370c3
PAMCH <pamic7124@naver.com>
2023-02-14 23:25:20 +0900

    Solved

---
+++ ex18-1/ex18-1/ex18-1.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+
+
+int main()
+{
+	char a = (1 << 7) - 1;
+	short b = (1 << 15) - 1;
+	int c = (1 << 31) - 1;
+	long long d = (1LL << 63) - 1;
+
+	printf("%hhd\n%hd\n%d\n%lld\n", a, b, c, d);
+
+	return 0;
+}
