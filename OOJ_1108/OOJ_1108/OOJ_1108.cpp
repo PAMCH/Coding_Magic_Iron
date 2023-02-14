b064a7f44aed2fef6d0ed08cb400da22ff67a31f
PAMCH <pamic7124@naver.com>
2023-02-14 23:51:05 +0900

    Solved

---
+++ OOJ_1108/OOJ_1108/OOJ_1108.cpp
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int gcd(int x, int y) {
+	while (y)
+	{
+		int r = x % y;
+		x = y;
+		y = r;
+	}
+
+	return x;
+}
+
+int main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+	printf("%d %d\n", a * b / gcd(a, b), gcd(a, b));
+
+	return 0;
+}
