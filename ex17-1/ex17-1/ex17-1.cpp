3bd3f6b62deadf84d599f6838c25bc4244c29cc3
PAMCH <pamic7124@naver.com>
2023-02-14 23:07:42 +0900

    Solved

---
+++ ex17-1/ex17-1/ex17-1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define MAX2(X, Y) ( (X) > (Y) ? (X) : (Y) )
+#define MIN2(X, Y) ( (X) < (Y) ? (X) : (Y) )
+
+
+int main()
+{
+	int a, b, c;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	printf("%d\n", MAX2(MAX2(a, b), c));
+	printf("%d\n", MIN2(MIN2(a, b), c));
+
+	return 0;
+}
