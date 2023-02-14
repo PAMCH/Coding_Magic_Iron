417d7bf2328a290755c14f869f4f83fa59f2e54d
PAMCH <pamic7124@naver.com>
2023-02-14 22:56:28 +0900

    Solved

---
+++ OOJ_2049/OOJ_2049/OOJ_2049.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int n;
+
+int reverse(int x) {
+	int r = 0;
+	while (x)
+	{
+		r = r * 10 + x % 10;
+		x /= 10;
+	}
+	return r;
+}
+
+int main()
+{
+	int cnt = 0;
+
+	scanf("%d", &n);
+
+	while (n != reverse(n)) // 팔린드롬
+	{
+		n = n + reverse(n);
+		cnt++;
+	}
+	printf("%d %d\n", cnt, n);
+	return 0;
+}
