889b0b62d79351f8f5c77fa66dc862a02d2d807e
PAMCH <pamic7124@naver.com>
2023-02-14 23:28:37 +0900

    Solved

---
+++ OOJ_1081/OOJ_1081/OOJ_1081.cpp
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+const int n = 6;
+const int a[n + 1] = { 0,500,100,50,10,5,1 };
+
+int moneyCnt(int money) {
+	int cnt = 0;
+
+	for (int i = 1; i <= n; i++) {
+		cnt += money / a[i];
+		money %= a[i];
+	}
+
+	return cnt;
+}
+
+int main()
+{
+	int money;
+
+	scanf("%d", &money);
+
+	printf("%d\n", moneyCnt(1000 - money));
+
+	return 0;
+}
