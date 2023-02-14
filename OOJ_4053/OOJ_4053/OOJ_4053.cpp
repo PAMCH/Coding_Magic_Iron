2df074a5a9ababf595bd4535dac30846533306b4
PAMCH <pamic7124@naver.com>
2023-02-14 23:58:37 +0900

    Solved

---
+++ OOJ_4053/OOJ_4053/OOJ_4053.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 10
+
+int n, a[N];
+
+int gcd(int x, int y) {
+	while (y)
+	{
+		int r = x % y;
+		x = y;
+		y = r;
+	}
+
+	return x;
+}
+
+int lcm(int x, int y) {
+	return x * y / gcd(x, y);
+}
+
+int main()
+{
+	int G, L;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++) {
+		scanf("%d", &a[i]);
+	}
+
+	G = L = a[1];
+
+	for (int i = 2; i <= n; i++) {
+		G = gcd(G, a[i]);
+		L = lcm(L, a[i]);
+	}
+
+	printf("%d\n%d\n", G, L);
+
+	return 0;
+}
