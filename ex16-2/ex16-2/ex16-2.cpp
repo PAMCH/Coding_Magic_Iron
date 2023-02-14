1c91a61245952d71a14c64ec6b0c336fc12d49af
PAMCH <pamic7124@naver.com>
2023-02-14 22:34:48 +0900

    Solved

---
+++ ex16-2/ex16-2/ex16-2.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+void magic(int a, int b, int c = 30) {
+	printf("%d %.2lf\n", a + b + c, double(a + b + c) / 3);
+}
+
+int main()
+{
+	magic(100, 88, 99);
+	magic(80, 92, 85);
+	magic(95, 88);
+	magic(70, 100);
+	magic(75, 85, 50);
+
+	return 0;
+}
