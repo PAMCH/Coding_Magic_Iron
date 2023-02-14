08432d998e8f17fcd7b2af53d3e627655779055c
PAMCH <pamic7124@naver.com>
2023-02-14 23:12:13 +0900

    Solved

---
+++ OOJ_1124/OOJ_1124/OOJ_1124.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define MIN2(X,Y) ((X)<(Y)?(X):(Y))
+
+int k;
+
+int main()
+{
+	int n, m;
+
+	scanf("%d", &k);
+
+	for (int i = 1; i <= k; i++) {
+		scanf("%d %d", &n, &m);
+		printf("%d\n", MIN2(n, m) * 2 - 2);
+	}
+
+	return 0;
+}
