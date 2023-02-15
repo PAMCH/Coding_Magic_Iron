f5ffb6ddad6d36ff6d94e58a2f8bc7e0fc55ebeb
PAMCH <pamic7124@naver.com>
2023-02-15 23:05:36 +0900

    Solved

---
+++ OOJ_2029/OOJ_2029/OOJ_2029.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 10000
+
+int n, check[N + 1];
+
+void input() {
+	scanf("%d", &n);
+}
+
+void core() {
+	int i = 0;
+
+	while (check[n] == 0) {
+		check[n] = ++i;
+		int n1 = (n / 100) % 10;
+		int n2 = (n / 10) % 10;
+		n = (n1 * 10 + n2) * (n1 * 10 + n2);
+	}
+
+	printf("%d\n", i);
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
