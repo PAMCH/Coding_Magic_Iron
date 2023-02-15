21c9feaff8a84e0b9cf5066e9b2274c2bd0c8718
PAMCH <pamic7124@naver.com>
2023-02-15 22:54:04 +0900

    Solved

---
+++ ex22-2/ex22-2/ex22-2.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+	int a, b;
+	int* ptr1, * ptr2;
+
+	scanf("%d %d", &a, &b);
+
+	ptr1 = &a;
+	ptr2 = &b;
+
+	(*ptr1)--;
+	(*ptr2)--;
+
+	ptr1 = &b;
+	ptr2 = &a;
+
+	printf("%d %d\n", *ptr1, *ptr2);
+
+	return 0;
+}
