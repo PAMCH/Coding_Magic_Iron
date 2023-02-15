26983254687042b2461820067c18aa76d5aa64f2
PAMCH <pamic7124@naver.com>
2023-02-15 23:37:34 +0900

    Solved

---
+++ OOJ_2032/OOJ_2032/OOJ_2032.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 100
+
+int n, a[N + 1], check[N + 1], cnt;
+
+void input() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+		scanf("%d", &a[i]);
+}
+
+void core() {
+	for(int i=1;i<=n;i++)
+		if (check[i] == 0) {
+			cnt++;
+			check[i] = 1;
+			for (int num = a[i]; num != i; num = a[num])
+				check[num] = 1;
+		}
+	printf("%d\n", cnt);
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
