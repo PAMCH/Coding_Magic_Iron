e7c20cad02b017588a4d0b7ba5b6b7cfe73f7d5b
PAMCH <pamic7124@naver.com>
2023-02-15 21:46:36 +0900

    Modified

---
+++ OOJ_2139/OOJ_2139/OOJ_2139.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 100
+
+int n, G, r[N + 1], g[N * 2 + 1];
+
+void input() {
+	scanf("%d %d", &n, &G);
+	for (int i = 1; i <= G; i++) {
+		scanf("%d", &g[i]);
+	}
+}
+
+void core() {
+	int gNum = 1;
+
+	for (int i = 1; i <= n && gNum <= G; i++) {
+		if (g[gNum] >= 2)
+			r[i] = 2, g[gNum] -= 2;
+		else if (g[gNum] == 1)
+			r[i] = 1, g[gNum] -= 1;
+
+		if (gNum <= G && g[gNum] == 0)
+			gNum++;
+	}
+
+	for (int i = 1; i <= n && gNum <= G; i++) {
+		if (r[i] == 1 && g[gNum] > 0)
+			r[i]++, g[gNum]--;
+
+		if (gNum <= G && g[gNum] == 0)
+			gNum++;
+	}
+}
+
+void output() {
+	for (int i = 1; i <= n; i++) {
+		printf("%d\n", r[i]);
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
