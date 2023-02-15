d326a5a9e3e4830cf8c1be9751965d292d9939b5
PAMCH <pamic7124@naver.com>
2023-02-15 22:30:50 +0900

    Modified

---
+++ ex21-1/ex21-1/ex21-1.cpp
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int n = 10;
+	int* ptr1, * ptr2;
+
+	ptr1 = &n;
+	ptr2 = ptr1;
+	printf("%d\n%d\n%d\n", (*ptr1)++, (*ptr2)++, n);
+
+	return 0;
+}
