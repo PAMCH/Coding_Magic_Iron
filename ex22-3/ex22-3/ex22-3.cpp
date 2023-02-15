8e323cc502889745d3785b50dafdad0a3d3352e8
PAMCH <pamic7124@naver.com>
2023-02-15 22:55:32 +0900

    Solved

---
+++ ex22-3/ex22-3/ex22-3.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	int a[5] = { 1,2,3,4,5 };
+	int* ptr = a;
+
+	for (int i = 0; i < 5; i++)
+		*(ptr + i) += 2;
+
+	for (int i = 0; i < 5; i++)
+		printf("%d ", *(ptr + i));
+
+	return 0;
+}
