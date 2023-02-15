a2cb994dba1b003f01b7439290b56d4feb959f55
PAMCH <pamic7124@naver.com>
2023-02-15 23:11:05 +0900

    Solved

---
+++ OOJ_2063/OOJ_2063/OOJ_2063.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 250000
+
+int n, e, check[N + 1], p[10];
+
+void input() {
+	scanf("%d %d", &n, &e);
+	for (int i = 1; i <= 9; i++) {
+		p[i] = int(pow(i, e));
+	}
+}
+
+void core() {
+	int i = 0;
+
+	while (check[n]==0)
+	{
+		int num = 0;
+		check[n] = ++i;
+		while (n !=0)
+		{
+			num += p[n % 10];
+			n /= 10;
+		}
+		n = num;
+	}
+
+	printf("%d\n", check[n] - 1);
+}
+
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
