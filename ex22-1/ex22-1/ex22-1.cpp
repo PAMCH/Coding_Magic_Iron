285ce157aecf4b53f8a94ee8219f81ae7a580e6f
PAMCH <pamic7124@naver.com>
2023-02-15 22:51:29 +0900

    Modified

---
+++ ex22-1/ex22-1/ex22-1.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	char a = 'A';
+	int b = 10;
+	double c = 3.8;
+
+	char* ptr1 = &a;
+	int* ptr2 = &b;
+	double* ptr3 = &c;
+
+	(*ptr1)++;
+	(*ptr2)++;
+	(*ptr3)++;
+
+	printf("%c %d %.1f", a, b, c);
+
+	return 0;
+}
