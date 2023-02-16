0c80b002aab69be712f694422392924acb1cbdd4
PAMCH <pamic7124@naver.com>
2023-02-16 23:43:07 +0900

    Solved

---
+++ OOJ_1051/OOJ_1051/OOJ_1051.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 30
+
+char a[31], b[31];
+
+void input() {
+    scanf("%s %s", a, b);
+}
+
+int main()
+{
+    input();
+    printf("%s, %s\n", b, a);
+
+    return 0;
+}
