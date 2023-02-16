4c25fc57ac0a79fb0c6560bb1af94ac6201210f4
PAMCH <pamic7124@naver.com>
2023-02-16 23:18:48 +0900

    Solved

---
+++ ex24-1/ex24-1/ex24-1.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+void output(const int* ptr) {
+    printf("%d\n", *ptr);
+}
+
+int main()
+{
+    const int a = 10;
+
+    output(&a);
+
+    return 0;
+}
