7380ebde194bad412b4805592ee4c1a489352b7e
PAMCH <pamic7124@naver.com>
2023-02-16 23:45:55 +0900

    Solved

---
+++ OOJ_1062/OOJ_1062/OOJ_1062.cpp
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int n;
+char a[4];
+
+void input() {
+    scanf("%d", &n);
+    scanf("%s", a);
+}
+
+void output() {
+    printf("%c\n", a[2]);
+    for (int i = 1; i <= n - 2; i++) {
+        printf("%c", a[0]);
+        for (int j = 1; j <= i - 1; j++) {
+            printf(" ");
+        }
+        printf("%c\n", a[2]);
+    }
+
+    printf("%c", a[0]);
+
+    for (int i = 1; i <= n - 1; i++) {
+        printf("%c", a[1]);
+    }
+}
+
+int main()
+{
+    input();
+    output();
+
+    return 0;
+}
