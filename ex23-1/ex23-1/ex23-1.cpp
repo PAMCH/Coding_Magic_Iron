fd89aa97383dfe608cbbff1b500d57ec0e0e3ab1
PAMCH <pamic7124@naver.com>
2023-02-16 22:54:43 +0900

    Solved

---
+++ ex23-1/ex23-1/ex23-1.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int n, a[101];
+
+void input() {
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+}
+
+int max(int* start, int* end) {
+    int max = *start;
+    while (start != end)
+    {
+        if (max < *start) {
+            max = *start;
+        }
+        start++;
+    }
+
+    return max;
+}
+int main()
+{
+    input();
+    printf("%d\n", max(a + 1, a + n + 1));
+
+    return 0;
+}
