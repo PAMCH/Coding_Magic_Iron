56ae9340dd07b6842192bc313b07758bd0100202
PAMCH <pamic7124@naver.com>
2023-02-16 23:32:04 +0900

    Solved

---
+++ OOJ_2014/OOJ_2014/OOJ_2014.cpp
@@ -0,0 +1,45 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int n;
+
+void input() {
+    scanf("%d", &n);
+}
+
+void core1(int* const b) {
+    for (int i = 1, sum = 0; i <= n; i++) {
+        sum += i;
+        if (sum >= n) {
+            *b = i;
+            break;
+        }
+    }
+}
+
+void core2(int* const t, int* const b) {
+    for (int i = *b, sum = 0; i >= 1; i--) {
+        sum += i;
+        if (sum >= n) {
+            *t = i - (sum - n);
+            break;
+        }
+    }
+}
+
+void output() {
+    int top, bot;
+
+    core1(&bot);
+    core2(&top, &bot);
+    printf("%d %d\n", bot, top);
+}
+
+int main()
+{
+    input();
+    output();
+
+    return 0;
+}
