22968d00b65fa129fea2044d6fac20319f4b98aa
PAMCH <pamic7124@naver.com>
2023-02-16 23:13:40 +0900

    Solved

---
+++ OOJ_2006/OOJ_2006/OOJ_2006.cpp
@@ -0,0 +1,43 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 99
+
+int n, a[N + 1];
+
+
+int sort(int* arr) {
+    for (int i = 1; i <= n - 1; i++) {
+        for (int j = i + 1; j <= n; j++) {
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    return arr[n / 2 + 1];
+}
+
+void input() {
+    int b[N + 1];
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            scanf("%d", &b[j]);
+        }
+        a[i] = sort(b);
+    }
+}
+
+int main()
+{
+    input();
+    printf("%d\n", sort(a));
+
+
+    return 0;
+}
