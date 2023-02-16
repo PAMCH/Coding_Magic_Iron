7d4cfc4bb00396a61bf05315363155c1ca67fdee
PAMCH <pamic7124@naver.com>
2023-02-16 23:40:54 +0900

    Solved

---
+++ ex25-1/ex25-1/ex25-1.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+    char a[11], b[12];
+
+    scanf("%s %s", a, b + 1);
+
+    printf("%s\n%s\n", a, b + 1);
+
+    return 0;
+}            
