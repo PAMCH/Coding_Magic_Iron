e952c2b3a77f7f41a8f5b5efafdb6b30edf15227
PAMCH <pamic7124@naver.com>
2023-02-16 22:59:54 +0900

    Solved

---
+++ ex23-2/ex23-2/ex23-2.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int n, s, e, a[101];
+
+void input() {
+    scanf("%d %d %d", &n, &s, &e);
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &a[i]);
+    }
+}
+
+void reverse(int* start, int* end) {
+    while (start < end)
+    {
+        int temp = *start;
+        *start = *(end - 1);
+        *(end - 1) = temp;
+        start++;
+        end--;
+    }
+}
+
+void output() {
+    for (int i = 1; i <= n; i++)
+        printf("%d ", a[i]);
+}
+
+int main()
+{
+    input();
+    reverse(a + s, a + e + 1);
+    output();
+
+    return 0;
+}
