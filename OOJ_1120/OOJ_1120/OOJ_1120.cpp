c1712eae4857f9008907d65d7eb977cc20a0322a
PAMCH <pamic7124@naver.com>
2023-02-17 21:49:30 +0900

    Solved

---
+++ OOJ_1120/OOJ_1120/OOJ_1120.cpp
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 13
+
+char a[N + 1];
+int n, sum;
+
+void input() {
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++) {
+		scanf("%s", a);
+		for (int j = 0; j < N; j++) {
+			if (a[j] == 'A')
+				sum += 4;
+			else if (a[j] == 'K')
+				sum += 3;
+			else if (a[j] == 'Q')
+				sum += 2;
+			else if (a[j] == 'J')
+				sum += 1;
+		}
+	}
+}
+
+int main()
+{
+	input();
+	printf("%d\n", sum);
+
+	return 0;
+}
