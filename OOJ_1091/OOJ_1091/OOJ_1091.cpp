827d229278c65c56f882f978faf29beff57144f4
PAMCH <pamic7124@naver.com>
2023-02-17 22:50:59 +0900

    Solved

---
+++ OOJ_1091/OOJ_1091/OOJ_1091.cpp
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define N 20
+
+char a[N + 1];
+int r;
+
+void input() {
+	scanf("%d %s", &r, a);
+}
+
+void output() {
+	for (int i = 0; i < strlen(a); i++) {
+		for (int j = 1; j <= r; j++) {
+			printf("%c", a[i]);
+		}
+	}
+	printf("\n");
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
