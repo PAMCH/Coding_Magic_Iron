7a662e572c9916dacc92c14c3f1022535731122d
PAMCH <pamic7124@naver.com>
2023-02-17 23:33:55 +0900

    Solved

---
+++ ex27-2/ex27-2/ex27-2.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+int func(const int& num1, const int& num2) {
+	return num1 + num2;
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+
+	printf("%d\n%d\n", func(a, b), func(10, 20));
+
+	return 0;
+}
