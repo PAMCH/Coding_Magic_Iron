fcf092076adde1cc340739a62dc00f53e29796e4
PAMCH <pamic7124@naver.com>
2023-02-17 23:46:40 +0900

    Solved

---
+++ OOJ_2072/OOJ_2072/OOJ_2072.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define N 10
+
+char a[N + 2];
+int idx, sum;
+
+void input() {
+	scanf("%s", &a[1]);
+}
+
+
+void core() {
+	for (int i = 1; i <= N; i++)
+		if (a[i] == '?')
+			idx = i;
+		else if (i == N && a[i] == 'X')
+			sum += N;
+		else
+			sum += (N - i + 1) * (a[i] - '0');
+}
+
+void output() {
+	int num;
+
+	if (idx == N && (sum + 10) % 11 == 0)
+		printf("X\n");
+	else
+	{
+		int num = -1;
+		for (int i = 0; i <= 9; i++)
+			if ((sum + (N - idx + 1) * i) % 11 == 0)
+				num = i;
+		printf("%d\n", num);
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
