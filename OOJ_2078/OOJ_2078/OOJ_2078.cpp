1bb48a26c0e32b8a66b5cdc14344138e412bdc1c
PAMCH <pamic7124@naver.com>
2023-02-17 23:39:32 +0900

    Solved

---
+++ OOJ_2078/OOJ_2078/OOJ_2078.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define N 1000
+
+char a[N + 2];
+int n;
+
+void input() {
+	scanf("%s", &a[1]);
+	n = strlen(&a[1]);
+}
+
+void core() {
+	int cnt = 1;
+
+	for (int i = 1; i <= n; i++) {
+		if (a[i] == a[i + 1])
+			cnt++;
+		else {
+			printf("%d%c", cnt, a[i]);
+			cnt = 1;
+		}
+	}
+}
+
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
