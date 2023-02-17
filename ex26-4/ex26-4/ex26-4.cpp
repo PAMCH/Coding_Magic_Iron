97a64257b463c84cd3a7ba2f86236d2fc50299a3
PAMCH <pamic7124@naver.com>
2023-02-17 22:38:07 +0900

    Solved

---
+++ ex26-4/ex26-4/ex26-4.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+void swap(char* str1, char* str2) {
+	char temp[11];
+
+	strcpy(temp, str1);
+	strcpy(str1, str2);
+	strcpy(str2, temp);
+}
+
+int main()
+{
+	char a[11], b[11];
+
+	scanf("%s", a);
+	scanf("%s", b);
+
+	printf("%s, %s\n", a, b);
+	swap(a, b);
+	printf("%s, %s\n", a, b);
+
+	return 0;
+}
