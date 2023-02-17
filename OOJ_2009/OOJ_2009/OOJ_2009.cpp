544f6aa85005370d5d820a77901285546947547d
PAMCH <pamic7124@naver.com>
2023-02-17 21:55:42 +0900

    Solved

---
+++ OOJ_2009/OOJ_2009/OOJ_2009.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+const char* str = "TINUKEDSOG";
+int n, m, cnt, a[10];
+
+void input() {
+	scanf("%d %d", &n, &m);
+}
+
+
+void core() {
+	int x = n + m;
+	while (x) {
+		a[++cnt] = x % 10;
+		x /= 10;
+	}
+}
+
+void output() {
+	for (int i = cnt; i >= 1; i--) {
+		printf("%c", str[a[i]]);
+	}
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
