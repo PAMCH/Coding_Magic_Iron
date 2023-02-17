7c110b867497300f46fa4caced769f0dfbec3089
PAMCH <pamic7124@naver.com>
2023-02-17 22:35:32 +0900

    Solved

---
+++ ex26-3/ex26-3/ex26-3.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	char a[11], b[11];
+
+	scanf("%s", a);
+	scanf("%s", b);
+
+	if (strcmp(a, b) < 0)
+		printf("Negative\n");
+	else if (strcmp(a, b) > 0)
+		printf("Positive\n");
+	else
+		printf("Zero\n");
+
+	return 0;
+}
