a5c3b74c0f4e6cceb95d1bb503e5361b7263515d
PAMCH <pamic7124@naver.com>
2023-02-17 22:44:03 +0900

    Solved

---
+++ OOJ_1052/OOJ_1052/OOJ_1052.cpp
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 10
+
+char a[N + 1];
+
+
+void input() {
+	scanf("%s", a);
+}
+
+void output() {
+	for (int i = 0; i < strlen(a); i++) {
+		printf("%c->%hhd\n", a[i], a[i]);
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
