de2b8eaaca06bfaea5e00a0e8873bf031aec41dc
PAMCH <pamic7124@naver.com>
2023-02-17 22:33:05 +0900

    Solved

---
+++ ex26-2/ex26-2/ex26-2.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+
+int main()
+{
+	char a[11];
+
+	scanf("%s", a);
+
+	for (int i = strlen(a) - 1; i >= 0; i--) {
+		printf("%c", a[i]);
+	}
+	printf("\n");
+
+	return 0;
+}
