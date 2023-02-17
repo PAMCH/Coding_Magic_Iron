19d97c470f673bec60364f44e3ad4fa63b6f5a69
PAMCH <pamic7124@naver.com>
2023-02-17 22:54:10 +0900

    Solved

---
+++ OOJ_1067/OOJ_1067/OOJ_1067.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define N 61
+
+int n;
+
+void input() {
+	scanf("%d", &n);
+}
+
+void output() {
+	for (int i = 1; i <= n; i++) {
+		char a[N + 1];
+
+		scanf("%s", a);
+		if (a[strlen(a) - 1] % 2)
+			printf("odd\n");
+		else
+			printf("even\n");
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
