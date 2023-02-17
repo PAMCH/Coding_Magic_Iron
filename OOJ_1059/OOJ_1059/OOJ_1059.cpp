8802e4b9da45a5b4df1acf59593db9cb3fc4b9f1
PAMCH <pamic7124@naver.com>
2023-02-17 22:48:22 +0900

    Solved

---
+++ OOJ_1059/OOJ_1059/OOJ_1059.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define N 6
+
+char a[N + 1], b[N + 1];
+int sum1 = 1, sum2 = 1;
+
+
+void input() {
+	scanf("%s", a);
+	scanf("%s", b);
+}
+
+
+void core() {
+	for (int i = 0; i < strlen(a); i++) {
+		sum1 *= (a[i] - 'A' + 1);
+	}
+	for (int i = 0; i < strlen(b); i++) {
+		sum2 *= (b[i] - 'A' + 1);
+	}
+}
+
+void output() {
+	if (sum1 % 47 == sum2 % 47)
+		printf("GO\n");
+	else
+		printf("STAY\n");
+}
+
+int main()
+{
+	input();
+	core();
+	output();
+
+	return 0;
+}
