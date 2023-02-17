760c6e9993c31a5641939b2b362614a74dc4c538
PAMCH <pamic7124@naver.com>
2023-02-17 22:39:20 +0900

    Solved

---
+++ ex26-5/ex26-5/ex26-5.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	char a[11], b[11];
+
+	scanf("%s", a);
+
+	for (int i = 0; i < strlen(a); i++) {
+		printf("%s\n", a + i);
+	}
+
+	return 0;
+}
