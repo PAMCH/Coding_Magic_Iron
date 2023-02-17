3da7bba29ba1226208a1ad1be9f552e9e9727a0a
PAMCH <pamic7124@naver.com>
2023-02-17 22:03:34 +0900

    Solved

---
+++ OOJ_2114/OOJ_2114/OOJ_2114.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#define N 50
+
+int n, cnt;
+char a[N + 2];
+
+void input() {
+	scanf("%d", &n);
+	scanf("%s", a + 1);
+}
+
+
+void core() {
+	for (int i = 1; i <= n; i++) {
+		if (a[i] == 'S') {
+			cnt++;
+		}
+		else if (a[i] == 'L') {
+			cnt++, i++;
+		}
+	}
+	cnt++;
+
+	printf("%d\n", n > cnt ? cnt : n);
+}
+
+int main()
+{
+	input();
+	core();
+
+	return 0;
+}
