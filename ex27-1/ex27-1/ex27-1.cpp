4acbb7cf81fd1f9eb57e9a9c077714cbc1404163
PAMCH <pamic7124@naver.com>
2023-02-17 23:32:34 +0900

    Solved

---
+++ ex27-1/ex27-1/ex27-1.cpp
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+void swap(int& num1, int& num2) {
+	int temp = num1;
+	num1 = num2;
+	num2 = temp;
+}
+
+int main()
+{
+	int a, b;
+
+	scanf("%d %d", &a, &b);
+
+	printf("%d %d\n", a, b);
+	swap(a, b);
+	printf("%d %d\n", a, b);
+
+	return 0;
+}
