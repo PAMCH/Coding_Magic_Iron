278920528c70897f05600f430256f14b328c3d65
PAMCH <pamic7124@naver.com>
2023-02-17 22:31:33 +0900

    Solved

---
+++ ex26-1/ex26-1/ex26-1.cpp
@@ -0,0 +1,17 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int main()
+{
+	char a[11];
+
+	scanf("%s", a);
+
+	if (strlen(a) % 2)
+		printf("odd\n");
+	else
+		printf("even\n");
+
+	return 0;
+}
