161e5474f81664312df6d4782492bc01aa9862b4
PAMCH <pamic7124@naver.com>
2023-02-18 13:26:42 +0900

    Solved

---
+++ ex28-3/ex28-3/ex28-3.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+void swap(int& x, int& y, int& z) {
+	int temp = z;
+	z = y;
+	y = x;
+	x = temp;
+}
+
+int main()
+{
+	int a, b, c;
+
+	scanf("%d %d %d", &a, &b, &c);
+
+	swap(a, b, c);
+	printf("%d %d %d\n", a, b, c);
+
+	return 0;
+}
