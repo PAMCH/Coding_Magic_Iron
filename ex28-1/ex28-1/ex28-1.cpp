22b6d3db03215fef55a431cf757f5465a9cd92f7
PAMCH <pamic7124@naver.com>
2023-02-18 13:11:30 +0900

    Solved

---
+++ ex28-1/ex28-1/ex28-1.cpp
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+
+int square(int x) {
+	return x * x;
+}
+
+int main()
+{
+	int a;
+
+	scanf("%d", &a);
+
+	a = square(a);
+	printf("%d\n", a);
+
+	return 0;
+}
