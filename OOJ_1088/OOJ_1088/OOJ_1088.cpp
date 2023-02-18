905ebb993b36fe78dedfc40626a3a2cf6b7cf992
PAMCH <pamic7124@naver.com>
2023-02-18 13:31:44 +0900

    Solved

---
+++ OOJ_1088/OOJ_1088/OOJ_1088.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+#define N 1000
+
+char a[N + 1];
+
+void input() {
+	scanf("%s", a);
+}
+
+char decode(char ch) {
+	if (ch >= 'D' && ch <= 'Z')
+		return ch - 3;
+	return ch + 23;
+}
+
+void output() {
+	for (int i = 0; i < strlen(a); i++) {
+		printf("%c", decode(a[i]));
+	}
+}
+
+int main()
+{
+	input();
+	output();
+
+	return 0;
+}
