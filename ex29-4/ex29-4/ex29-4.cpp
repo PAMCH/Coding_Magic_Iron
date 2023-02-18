fe542db9d967c5a3cb9af9375b1e52706cb07800
PAMCH <pamic7124@naver.com>
2023-02-18 15:02:33 +0900

    Solved

---
+++ ex29-4/ex29-4/ex29-4.cpp
@@ -0,0 +1,20 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+int main()
+{
+	int high, raw, col;
+
+	int dhigh[6] = { 0,0,0,0,-1,1 };
+	int draw[6] = { 0,1,0,-1,0,0 };
+	int dcol[6] = { 1,0,-1,0,0,0 };
+
+	scanf("%d %d %d", &high, &raw, &col);
+
+	for (int i = 0; i < 6; i++)
+		printf("%d %d %d\n", high+dhigh[i], raw + draw[i], col + dcol[i]);
+
+	return 0;
+}
