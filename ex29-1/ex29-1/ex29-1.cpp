fc2e9c8c09330cfa0e20f69d3a1666a49ba75142
PAMCH <pamic7124@naver.com>
2023-02-18 14:36:53 +0900

    Solved

---
+++ ex29-1/ex29-1/ex29-1.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+int main()
+{
+	int raw, col;
+
+	int draw[4] = { 0,1,0,-1 }, dcol[4] = { 1,0,-1,0 };
+
+	scanf("%d %d", &raw, &col);
+
+	for (int i = 0; i < 4; i++)
+		printf("%d %d\n", raw + draw[i], col + dcol[i]);
+
+	return 0;
+}
